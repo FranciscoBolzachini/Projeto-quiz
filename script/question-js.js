633cb82619cd627ff5cc75a7d44d3f1d5a6df245
Nicoli <nicoli.rodriguesslopes@gmail.com>
2023-07-27 14:34:26 -0300

    Criação do timer

---
+++ script/question-js.js
@@ -0,0 +1,63 @@
+export const quetionsJS = [
+    {
+        num: 1,
+        question:
+        "Qual é a linguagem de programação utilizada para criar interações em páginas web?",
+        answer: "C. JavaScript",
+        options: ["A. HTML", "B. CSS", "C. JavaScript", "D. Java"],
+    },
+    {
+        num: 2,
+        question: "Qual é a função do operador "+" no JavaScript?",
+        answer: "A. Adição",
+        options: ["A. Adição", "B. Concatenação", "C. Subtração", "D. Multiplicação"],
+    },
+    {
+        num: 3,
+        question:"Qual é a estrutura de controle utilizada para executar um bloco de código repetidamente em JavaScript?",
+        answer: "A. while",
+        options: ["A. whilw", "B. for", "C. if/else", "D. switch"],
+    },
+    {
+        num: 4,
+        question: "Qual é o método do JavaScript utilizado para exibir mensagens em uma caixa de diálogo?",
+        answer: "B. alert()",
+        options: ["A. prompt()", "B. alert()", "C. console.log()", "D. confirm()"],
+    },
+    {
+        num: 5,
+        question: "Qual é o objeto global do JavaScript que representa o documento HTML?",
+        answer: "A. document",
+        options: ["A. document", "B. window", "C. html", "D. DOM"],
+    },
+    {
+        num: 6,
+        question: "Qual é o método do JavaScript utilizado para converter um texto em número inteiro?",
+        answer: "A. parseInt()",
+        options: ["A. parseInt()", "B. toInt()", "C. parseNumber()", "D. convertInt()"],
+    },
+    {
+        num: 7,
+        question: "Qual é o operador lógico utilizado para verificar se duas condições são verdadeiras em JavaScript?",
+        answer: "B. &&",
+        options: ["A. ||", "B. &&", "C. !", "D. <>"],
+    },
+    {
+        num: 8,
+        question: "Qual é a estrutura de controle utilizada para tomar decisões com base em condições em JavaScript?",
+        answer: "D. if/else",
+        options: ["A. while", "B. switch", "C. for", "D. if/else"],
+    },
+    {
+        num: 9,
+        question: "Qual é o método do JavaScript utilizado para alterar o conteúdo de um elemento HTML?",
+        answer: "C. innerHTML()",
+        options: ["A. setElement()", "B. changeElement()", "C. innerHTML()", "D. updateElement()"],
+    },
+    {
+        num: 10,
+        question: "Qual é a função do operador '===' no JavaScript?",
+        answer: "D. Comparar valores e tipos",
+        options: ["A. Executar uma adição", "B. Comparar apenas valores", "C. Comparar apenas tipos", "D. Comparar valores e tipos"],
+    }
+];
