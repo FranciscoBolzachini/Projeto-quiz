ed581ea02ba45d7a53acc580615a6c99002c143d
Nicoli <nicoli.rodriguesslopes@gmail.com>
2023-07-31 18:31:36 -0300

    Imports

---
+++ script/quiz.js
@@ -0,0 +1,5 @@
+import { questionsHTML } from "./questions-html";
+import { questionsCSS } from "./questions-css";
+import { quetionsJS } from "./question-js";
+import { twoDigits, start, pause, timer } from "./timer";
+import { audios, } from "./musica";
